929c993faeb12e6d722fcd203b79f4681db79a86
kuldeep-98 <kuldeepbhama4455@gmail.com>
2021-03-28 04:18:43 +0530

    minor changes

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialDto } from './dto/auth-credentials.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(
+    @Body(ValidationPipe) authCredentialDto: AuthCredentialDto,
+  ): Promise<void> {
+    console.log(authCredentialDto);
+    return this.authService.signUp(authCredentialDto);
+  }
+}
